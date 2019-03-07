e8f52786c421608ba705dde88f24e4e4a56c9b2f
lasse <lasse-schloer@servermx.de>
2019-03-07 10:30:48 +0100

    incorporated include preprocessor

--- gist-generate/gist-generate.fish
+++ gist-generate/gist-generate.fish
@@ -2,13 +2,18 @@
 
 set --local initial_dir (pwd)
 set --local gist_dir ../targets/gist
+
+./asciidoc-include-preprocessor.fish
 rm -rf $gist_dir/*
-cp sin-cos-approximations-gist.adoc $gist_dir/
+cp out.adoc $gist_dir/sin-cos-approximations-gist.adoc
+rm out.adoc
 cd $gist_dir
+
 set --local commit_message (date)
 set commit_message (string join ""\
   "automated commit for newly generated gist (" (date) ")")
 git commit -am $commit_message
 git push
+
 cd $initial_dir
 
