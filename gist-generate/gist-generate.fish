054506d191db66ecb38ec1b006de45655899bcfa
lasse <lasse-schloer@servermx.de>
2019-03-07 08:53:59 +0100

    working gist target is on the way…

--- gist-generate/gist-generate.fish
+++ gist-generate/gist-generate.fish
@@ -1,10 +1,14 @@
 #!/usr/bin/env fish
 
-set --local wd (pwd)
-rm -rf ../gist/*
-cp sin-cos-approximations-gist.adoc ../gist/
-cd ../gist
-git commit -am "automated commit for newly generated gist \((date)\)"
+set --local initial_dir (pwd)
+set --local gist_dir ../targets/gist
+rm -rf $gist_dir/*
+cp sin-cos-approximations-gist.adoc $gist_dir/
+cd $gist_dir
+set --local commit_message (date)
+set commit_message (string join ""\
+  "automated commit for newly generated gist (" (date) ")")
+git commit -am $commit_message
 git push
-cd $wd
+cd $initial_dir
 
