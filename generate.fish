d1988de903fb753e58e1925262c8f45e7cade9da
lasse <lasse-schloer@servermx.de>
2019-03-07 06:54:17 +0100

    minor adjustments

--- generate.fish
+++ generate.fish
@@ -4,6 +4,7 @@
 set --local execute_scripts yes
 set --local doctype article
 set --local wolframscript_command wolframscript
+set --local target_dir targets
 
 for i in $argv
   if test $i = --skip-scripts
@@ -20,6 +21,8 @@ if test $execute_scripts = yes
   eval $wolframscript_command scripts/generate-error-summary-plot.wls
 end
 
-asciidoctor-pdf --doctype $doctype sin-cos-approximations.adoc
-asciidoctor --doctype $doctype sin-cos-approximations.adoc
+asciidoctor-pdf --doctype $doctype\
+  --out-file $target_dir/sin-cos-approximations.pdf sin-cos-approximations.adoc
+asciidoctor --doctype $doctype --attribute data-uri\
+  --out-file $target_dir/sin-cos-approximations.html sin-cos-approximations.adoc
 
